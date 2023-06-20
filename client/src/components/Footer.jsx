743b5e573753c55bd075be87362bf5fd8f8ef402
thtshajjad <shagors@users.noreply.github.com>
2023-06-20 17:15:55 +0600

    design header footer and home data pass and check it

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Logo from "../img/logo.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <img className="" src={Logo} alt="logo" />
+      <span>
+        Made with <b>React and MySQL</b>
+      </span>
+    </footer>
+  );
 };
 
 export default Footer;
