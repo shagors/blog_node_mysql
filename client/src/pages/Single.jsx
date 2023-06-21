01d208f1cce8b98ab75f8090f9809e1d66e101ab
thtshajjad <shagors@users.noreply.github.com>
2023-06-21 11:56:06 +0600

    design single page and add side and edit options add and edit design add

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Edit from "../img/edit.png";
 import Delete from "../img/delete.png";
 import { Link } from "react-router-dom";
+import Menu from "../components/Menu";
 
 const Single = () => {
   return (
@@ -75,7 +76,7 @@ const Single = () => {
           posuere convallis id
         </p>
       </div>
-      <div className="menu">m</div>
+      <Menu />
     </div>
   );
 };
